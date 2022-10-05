a52e1a54ce921ea1dd76dd2b25c37293d23334f1
Vijay Gupta <mevijaygupta2010@gmail.com>
2022-10-05 01:46:40 -0700

    broke on test case

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ app.include_router(auth.router)
 app.include_router(vote.router)
 @app.get("/")
 def root():
-    return{"message": "Welcome to my coolest Api!!"}
+    return{"message": "Welcome VJ FASTAPI WORLD!!"}
 
 
 
